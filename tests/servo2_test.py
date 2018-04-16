b77282de9c63724735fed10464ced2c1556d6186
chicken <delicious.chicken@gmail.com>
2018-04-15 19:55:40 -0700

    testing servos, rearranging

--- tests/servo2_test.py
+++ tests/servo2_test.py
@@ -2,9 +2,9 @@
 
 import smbus, time
 
-bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+bus = smbus.SMBus(1)	# 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
-DEVICE_ADDRESS = 0x04      #7 bit address (will be left shifted to add the read write bit)
+DEVICE_ADDRESS = 0x04	  #7 bit address (will be left shifted to add the read write bit)
 DEVICE_REG_MODE1 = 0x00
 DEVICE_REG_LEDOUT0 = 0x1d
 
@@ -16,11 +16,11 @@ while True:
 	var = input("Please enter a number 0-127: ")
 	print("You entered " + str(var))
 	var2 = input("Please enter another number 0-127: ")
-        
+		
 	ledout_values = [0,5]
-    print(ledout_values)
-    bus.write_i2c_block_data(DEVICE_ADDRESS, DEVICE_REG_LEDOUT0, ledout_values)
-    time.sleep(2)
+	print(ledout_values)
+	bus.write_i2c_block_data(DEVICE_ADDRESS, DEVICE_REG_LEDOUT0, ledout_values)
+	time.sleep(2)
 
 	ledout_values = [1, int(var), int(var2)]
 	print(ledout_values)
